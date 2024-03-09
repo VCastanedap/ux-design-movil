c65bdb31b12a110fa49a6ebca30e8bb2364dde49
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-03-09 11:32:01 -0500

    Add confirmation delete navigation

--- app/src/main/java/com/victor/coolcamara/ConfirmationDelete.kt
+++ app/src/main/java/com/victor/coolcamara/ConfirmationDelete.kt
@@ -1,15 +1,27 @@
 package com.victor.coolcamara
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.victor.coolcamara.ui.theme.CoolCamaraTheme
 
 class ConfirmationDelete : ComponentActivity() {
@@ -22,7 +34,7 @@ class ConfirmationDelete : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainConfirmationDelete()
                 }
             }
         }
@@ -30,10 +42,57 @@ class ConfirmationDelete : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainConfirmationDelete() {
+    Scaffold(
+        topBar = {
+            AppTopBar(toolBarTitle = "Confirmación")
+        },
+        content =
+        { padding ->
+            Surface(
+                modifier = Modifier.padding(padding),
+            ) {
+                Column(Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Row {
+                        Text(text = "¿Estás seguro de eliminar Alarma 1?")
+                    }
+
+                    Spacer(modifier = Modifier.size(width = 0.dp, height = 20.dp))
+
+                    Row{
+                        Column (horizontalAlignment = Alignment.Start){
+                            val context = LocalContext.current
+
+                            Button(
+                                onClick = {
+                                    context.startActivity(Intent(context, DeleteAlarm::class.java))
+                                },
+                                modifier =  Modifier.size(width = 120.dp,height = 45.dp)
+                            ) {
+                                Text(text = "Cancelar")
+                            }
+                        }
+
+                        Column (horizontalAlignment = Alignment.End){
+                            val context = LocalContext.current
+
+                            Button(
+                                onClick = {
+                                    context.startActivity(Intent(context, MainActivity::class.java))
+                                },
+                                modifier =  Modifier.size(width = 120.dp,height = 45.dp)
+                            ) {
+                                Text(text = "Eliminar")
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
     )
 }
 
@@ -41,6 +100,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview2() {
     CoolCamaraTheme {
-        Greeting("Android")
+        MainConfirmationDelete()
     }
 }
