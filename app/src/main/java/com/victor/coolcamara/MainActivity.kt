49a727259179c18773a93aab1c6efea7564fad42
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-03-09 10:19:02 -0500

    Navigation

--- app/src/main/java/com/victor/coolcamara/MainActivity.kt
+++ app/src/main/java/com/victor/coolcamara/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.victor.coolcamara
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.MoreVert
@@ -26,10 +26,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -40,13 +40,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.victor.coolcamara.ui.theme.CoolCamaraTheme
 
@@ -148,9 +149,20 @@ fun AlarmCard(){
         Row(
             modifier = Modifier.padding(all=5.dp)
         ) {
+
             var checked by remember { mutableStateOf(true) }
 
-            Spacer(modifier = Modifier.size(width = 280.dp, height = 0.dp))
+            Column(modifier = Modifier.padding(all=5.dp), horizontalAlignment = Alignment.Start) {
+                val context = LocalContext.current
+
+                IconButton(onClick = {
+                    context.startActivity(Intent(context, DeleteAlarm::class.java))
+                }) {
+                    Icon(imageVector = Icons.Filled.Delete, contentDescription = "DeleteButton")
+                }
+            }
+
+            Spacer(modifier = Modifier.size(width = 230.dp, height = 0.dp))
 
             Switch(
                 checked = checked,
@@ -200,24 +212,23 @@ fun MainPreview() {
 @Composable
 fun AppPaginator() {
     Row {
-        Column() {
+        Column {
             IconButton(onClick = {  }) {
                 Icon(imageVector = Icons.Filled.KeyboardArrowLeft, contentDescription = "Left")
             }
         }
 
-        Column() {
+        Column {
             LinearProgressIndicator(progress = 0.7f)
         }
 
-        Column() {
+        Column {
             IconButton(onClick = {  }) {
                 Icon(imageVector = Icons.Filled.KeyboardArrowRight, contentDescription = "Right     ")
             }
         }
     }
 }
-
 
 
 @Composable
