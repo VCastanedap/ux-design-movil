4972bf416f43bc9495ad1d8d1a5e0fc9f565e952
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-03-09 11:11:19 -0500

    Add delete button

--- app/src/main/java/com/victor/coolcamara/DeleteAlarm.kt
+++ app/src/main/java/com/victor/coolcamara/DeleteAlarm.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,6 +61,14 @@ fun DeleteMain() {
                     Spacer(modifier = Modifier.size(width = 0.dp, height = 20.dp))
 
                     Row{
+                        Column (horizontalAlignment = Alignment.CenterHorizontally){
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier =  Modifier.size(width = 220.dp,height = 45.dp)
+                            ) {
+                                Text(text = "Eliminar")
+                            }
+                        }
 
                     }
                 }
